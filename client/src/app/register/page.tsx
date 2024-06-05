852957f7b87f65deadef3eb2c688287a75989d76
RZajacc <rf.zajac@gmail.com>
2024-06-05 16:26:02 +0200

    uncommented old version of register component and cleaned it up from elements that wont be used and from bootstrap

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,173 +1,95 @@
-// import { Button, Container, Form } from "react-bootstrap";
-// import { ChangeEvent, FormEvent, useState, useContext, useEffect } from "react";
-// import { User } from "../types/types";
-// import "../styles/accountPage.css";
-// import { AuthContext } from "../context/AuthContext";
+import { ChangeEvent, FormEvent, useState, useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
-// type Props = {
-//   setLogReg: (status: string) => void;
-// };
+type Props = {};
 
-// function Register({ setLogReg }: Props) {
-//   // *Setting up context
-//   const { registerWithEmail } = useContext(AuthContext);
+function Register({}: Props) {
+  // *Setting up context
+  const {} = useContext(AuthContext);
 
-//   // *1. Setting up a new user from input fields
-//   const [newUser, setNewUser] = useState<User>({
-//     _id: "",
-//     userName: "",
-//     email: "",
-//     password: "",
-//     userImage: "",
-//     userWebsite: "",
-//     userBio: "",
-//     favs: [""],
-//     posts: [""],
-//   });
-//   const [confirmPassword, setConfirmPassword] = useState("");
-//   const [passwordErr, setPasswordErr] = useState<string[] | null>(null);
+  // *4 Register a new user
+  const handleRegisterSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //   ! Walidacja do osobnego pliku i tylko wynik tutaj, form data
+    //   ! Rejstracja z contextu, na backendzie zmniejsz nowego użytkownika do trzech pól które ma
 
-//   // *2. Receiving input from a form
-//   const handleInputData = (e: ChangeEvent<HTMLInputElement>) => {
-//     setNewUser({ ...newUser, [e.target.name]: e.target.value });
-//   };
+    // const formValidation: string[] = [];
 
-//   const handleConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
-//     setConfirmPassword(e.target.value);
-//   };
+    // if (newUser.password !== confirmPassword) {
+    //   formValidation.push("Provided passwords don't match!");
+    // } else {
+    //   if (newUser.password.length < 8 || confirmPassword.length < 8) {
+    //     formValidation.push('Password is too short!');
+    //   }
 
-//   // *3. Change displayed element on the page
-//   const handleChangeState = () => {
-//     setLogReg("login");
-//   };
+    //   if (!/[A-Z]/.test(newUser.password) || !/[A-Z]/.test(confirmPassword)) {
+    //     formValidation.push(
+    //       'Password needs to have at least one uppercase letter!',
+    //     );
+    //   }
 
-//   // *4 Register a new user
-//   const handleRegisterSubmit = (e: FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const formValidation: string[] = [];
+    //   if (
+    //     !/[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(newUser.password) ||
+    //     !/[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(confirmPassword)
+    //   ) {
+    //     formValidation.push(
+    //       'Password needs to contain at least 1 special character!',
+    //     );
+    //   }
 
-//     if (newUser.password !== confirmPassword) {
-//       formValidation.push("Provided passwords don't match!");
-//     } else {
-//       if (newUser.password.length < 8 || confirmPassword.length < 8) {
-//         formValidation.push("Password is too short!");
-//       }
+    //   if (!/[0-9]/.test(newUser.password) || !/[0-9]/.test(confirmPassword)) {
+    //     formValidation.push('Password needs to contain at least one number!');
+    //   }
+    // }
 
-//       if (!/[A-Z]/.test(newUser.password) || !/[A-Z]/.test(confirmPassword)) {
-//         formValidation.push(
-//           "Password needs to have at least one uppercase letter!"
-//         );
-//       }
+    // setPasswordErr(formValidation);
 
-//       if (
-//         !/[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(newUser.password) ||
-//         !/[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/g.test(confirmPassword)
-//       ) {
-//         formValidation.push(
-//           "Password needs to contain at least 1 special character!"
-//         );
-//       }
+    // if (passwordErr?.length === 0) {
+    //   registerWithEmail(newUser);
+    //   setLogReg('login');
+    // }
+  };
 
-//       if (!/[0-9]/.test(newUser.password) || !/[0-9]/.test(confirmPassword)) {
-//         formValidation.push("Password needs to contain at least one number!");
-//       }
-//     }
+  return (
+    <>
+      <div className="welcome-div">
+        <h4>Welcome to TotArt</h4>
+        <p>To use all of our functionalities please register a new account</p>
+        {/* Link to lgin */}
+        <p className="logreg-paragraph">
+          If you already have an one then simply{' '}
+        </p>
+      </div>
+      <form action="">
+        <label htmlFor="username">Username:</label>
+        <input type="text" required name="username" placeholder="username" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" required name="email" placeholder="john@doe.com" />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          required
+          name="password"
+          placeholder="password"
+        />
+        <label htmlFor="confirm-password">Confirm password:</label>
+        <input
+          type="password"
+          required
+          name="confirm-password"
+          placeholder="password"
+        />
+        <button type="submit">Register</button>
+      </form>
 
-//     setPasswordErr(formValidation);
+      <div className="password-req">
+        <p>*Password needs to have at least 8 characters</p>
+        <p>*Password needs to contain at least 1 uppercase character</p>
+        <p>*Password needs to contain at least 1 number</p>
+        <p>*Password needs to contain at least 1 special character</p>
+      </div>
+    </>
+  );
+}
 
-//     if (passwordErr?.length === 0) {
-//       registerWithEmail(newUser);
-//       setLogReg("login");
-//     }
-//   };
-
-//   return (
-//     <>
-//       <Container className="account-container">
-//         <div className="welcome-div">
-//           <h4>Welcome to TotArt</h4>
-//           <p>To use all of our functionalities please register a new account</p>
-//           <p className="logreg-paragraph">
-//             If you already have an one then simply{" "}
-//             <span className="statusHandler" onClick={handleChangeState}>
-//               login
-//             </span>
-//           </p>
-//         </div>
-//         <Form onSubmit={handleRegisterSubmit}>
-//           <Form.Group className="mb-3" controlId="username">
-//             <Form.Label>Username:</Form.Label>
-//             <Form.Control
-//               type="text"
-//               name="userName"
-//               placeholder="Enter username"
-//               onChange={handleInputData}
-//               isValid={newUser.userName != ""}
-//               required
-//             />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3" controlId="user-email">
-//             <Form.Label>Email address</Form.Label>
-//             <Form.Control
-//               type="email"
-//               name="email"
-//               placeholder="Enter email"
-//               onChange={handleInputData}
-//               required
-//             />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3" controlId="user-password">
-//             <Form.Label>Password</Form.Label>
-//             <Form.Control
-//               type="password"
-//               name="password"
-//               placeholder="Password"
-//               onChange={handleInputData}
-//               autoComplete="user-password"
-//               isInvalid={passwordErr?.length != null && passwordErr.length != 0}
-//               isValid={passwordErr?.length === 0}
-//               required
-//             />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3" controlId="confirm-password">
-//             <Form.Label>Confirm password</Form.Label>
-//             <Form.Control
-//               type="password"
-//               name="password"
-//               placeholder="Password"
-//               onChange={handleConfirmPassword}
-//               autoComplete="user-password"
-//               isInvalid={passwordErr?.length != null && passwordErr.length != 0}
-//               isValid={passwordErr?.length === 0}
-//               required
-//             />
-//             {passwordErr &&
-//               passwordErr.map((err, idx) => {
-//                 return (
-//                   <Form.Control.Feedback type="invalid" key={idx}>
-//                     {err}
-//                   </Form.Control.Feedback>
-//                 );
-//               })}
-//           </Form.Group>
-//           <div className="text-center">
-//             <Button variant="warning" type="submit">
-//               Register
-//             </Button>
-//           </div>
-//           <div className="password-req">
-//             <p>*Password needs to have at least 8 characters</p>
-//             <p>*Password needs to contain at least 1 uppercase character</p>
-//             <p>*Password needs to contain at least 1 number</p>
-//             <p>*Password needs to contain at least 1 special character</p>
-//           </div>
-//         </Form>
-//       </Container>
-//     </>
-//   );
-// }
-
-// export default Register;
+export default Register;
