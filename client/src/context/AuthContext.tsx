946fa8675a85d0b76041e9a05f4793c3ac8405af
RZajacc <rf.zajac@gmail.com>
2024-06-05 13:54:14 +0200

    moved some functionality in auth context from separate functions to useEffect directly to improve readability since they both run in the useEffect anyway

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -29,90 +29,73 @@ export const AuthContextProvider = ({ children }: AuthContexProviderProps) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
-  // *1_REGISTER A NEW USER
-  const registerWithEmail = async (newUser: User) => {
-    const urlencoded = new URLSearchParams();
-    urlencoded.append('userName', newUser.userName);
-    urlencoded.append('email', newUser.email);
-    urlencoded.append('password', newUser.password);
-    urlencoded.append(
-      'userImage',
-      'https://res.cloudinary.com/dqdofxwft/image/upload/v1698072044/other/nil6d9iaml3c6hqfdhly.png',
-    );
-    urlencoded.append('userWebsite', '');
-    urlencoded.append('userBio', '');
+  // 1_REGISTER A NEW USER
+  // const registerWithEmail = async (newUser: User) => {
+  //   const urlencoded = new URLSearchParams();
+  //   urlencoded.append('userName', newUser.userName);
+  //   urlencoded.append('email', newUser.email);
+  //   urlencoded.append('password', newUser.password);
+  //   urlencoded.append(
+  //     'userImage',
+  //     'https://res.cloudinary.com/dqdofxwft/image/upload/v1698072044/other/nil6d9iaml3c6hqfdhly.png',
+  //   );
+  //   urlencoded.append('userWebsite', '');
+  //   urlencoded.append('userBio', '');
 
-    const requestOptions = {
-      method: 'POST',
-      body: urlencoded,
-    };
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     body: urlencoded,
+  //   };
 
-    try {
-      const response = await fetch(
-        'http://localhost:5000/api/users/register',
-        requestOptions,
-      );
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //   try {
+  //     const response = await fetch(
+  //       'http://localhost:5000/api/users/register',
+  //       requestOptions,
+  //     );
+  //     const result = await response.json();
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  // *2_GET USER WITH A TOKEN
-  const getUser = async (myToken: string) => {
-    if (myToken) {
-      const myHeaders = new Headers();
-      myHeaders.append('Authorization', `Bearer ${myToken}`);
-
-      const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-      };
-
-      try {
-        const response = await fetch(
-          'http://localhost:5000/api/users/profile',
-          requestOptions,
-        );
-        if (response.ok) {
-          const result = await response.json();
-          const user = result.user as User;
-          return user;
-        }
-      } catch (err) {
-        const error = err as Error;
-        console.log(error.message);
-      }
-    }
-  };
-
-  // *3_CHECK IF USER IS LOGGED IN
-  const isUserLoggedIn = async () => {
-    const token = getCookie('auth_token');
-    if (token) {
-      const user: User | undefined = await getUser(token);
-      if (user) {
-        setUser(user);
-        setIsLoggedIn(true);
-      }
-    } else {
-      setIsLoggedIn(false);
-      setUser(null);
-    }
-  };
-
-  useEffect(() => {
-    isUserLoggedIn();
-  }, [isLoggedIn]);
-
-  // *LOGOUT
+  // LOGOUT
   const logout = () => {
     deleteCookie('auth_token');
     setUser(null);
     setIsLoggedIn(false);
     router.push('/login');
   };
+
+  useEffect(() => {
+    // Get token from cookie
+    const token = getCookie('auth_token');
+
+    // If token exists get user data
+    if (token) {
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization', `Bearer ${token}`);
+
+      fetch('http://localhost:5000/api/users/profile', {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          const user: User | undefined = result.user;
+          if (user) {
+            setUser(user);
+            setIsLoggedIn(true);
+          } else {
+            setIsLoggedIn(false);
+            setUser(null);
+          }
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [isLoggedIn]);
+
   return (
     <AuthContext.Provider
       value={{
