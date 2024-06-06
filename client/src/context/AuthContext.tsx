1d59a726822bbd3a1f92118a80a9f09104162b12
RZajacc <rf.zajac@gmail.com>
2024-06-06 14:51:27 +0200

    deleted user registration code from the context

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -29,36 +29,6 @@ export const AuthContextProvider = ({ children }: AuthContexProviderProps) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
-  // 1_REGISTER A NEW USER
-  // const registerWithEmail = async (newUser: User) => {
-  //   const urlencoded = new URLSearchParams();
-  //   urlencoded.append('userName', newUser.userName);
-  //   urlencoded.append('email', newUser.email);
-  //   urlencoded.append('password', newUser.password);
-  //   urlencoded.append(
-  //     'userImage',
-  //     'https://res.cloudinary.com/dqdofxwft/image/upload/v1698072044/other/nil6d9iaml3c6hqfdhly.png',
-  //   );
-  //   urlencoded.append('userWebsite', '');
-  //   urlencoded.append('userBio', '');
-
-  //   const requestOptions = {
-  //     method: 'POST',
-  //     body: urlencoded,
-  //   };
-
-  //   try {
-  //     const response = await fetch(
-  //       'http://localhost:5000/api/users/register',
-  //       requestOptions,
-  //     );
-  //     const result = await response.json();
-  //     console.log(result);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   // LOGOUT
   const logout = () => {
     deleteCookie('auth_token');
