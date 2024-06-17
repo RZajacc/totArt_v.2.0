a646c0c50647b1f75c86e6c90c038f89ab3dbbce
RZajacc <rf.zajac@gmail.com>
2024-06-17 16:47:33 +0200

    created a typescript type for image

--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -66,3 +66,24 @@ export type editFieldStatus = {
   editField: boolean;
   submitField: boolean;
 };
+
+export type Image = {
+  asset_id: string;
+  public_id: string;
+  version: number;
+  version_id: string;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  created_at: string;
+  bytes: number;
+  type: string;
+  etag: string;
+  placeholder: boolean;
+  url: string;
+  secure_url: string;
+  folder: string;
+  original_filename: string;
+};
