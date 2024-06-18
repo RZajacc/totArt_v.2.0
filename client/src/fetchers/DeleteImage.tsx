c334d17e9f22d956be3f783d84848461092b0d80
RZajacc <rf.zajac@gmail.com>
2024-06-18 16:34:56 +0200

    added a delete image fetch method in fetcher folder

---
+++ client/src/fetchers/DeleteImage.tsx
@@ -0,0 +1,32 @@
+import { FetchError } from '../types/types';
+
+export const deleteImage = async (
+  url: string,
+  { arg }: { arg: { publicId: string } },
+) => {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+  // const url = 'http://localhost:5000/api/images/imageDelete'
+  const urlencoded = new URLSearchParams();
+  urlencoded.append('publicId', arg.publicId);
+
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow',
+  });
+
+  if (response.ok) {
+    const result: { message: string; userImage: { result: string } } =
+      await response.json();
+    return result;
+  } else {
+    const result: { msg: string } = await response.json();
+    const error: FetchError = new Error('Failed to delete the image');
+    error.info = result.msg;
+    error.status = response.status;
+    throw error;
+  }
+};
