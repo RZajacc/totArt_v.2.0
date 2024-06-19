24bb1ef7fdecea2ad5ff4e6952f318657da2791f
RZajacc <rf.zajac@gmail.com>
2024-06-19 17:27:01 +0200

    created an image type

--- client/src/types/locationTypes.tsx
+++ client/src/types/locationTypes.tsx
@@ -1,9 +1,9 @@
-export type post = {
+export type locationType = {
   _id: string;
   title: string;
   description: string;
   location: string;
-  imageUrl: string;
+  image: string;
   author: authorType;
   comments: [comment];
 };
