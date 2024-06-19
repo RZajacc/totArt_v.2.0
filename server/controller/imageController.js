c5733212fd11c6b603d5f6f4a3ea8dd7c11055b0
RZajacc <rf.zajac@gmail.com>
2024-06-19 13:57:18 +0200

    created a image model, and started to work on imageUpload controller

--- server/controller/imageController.js
+++ server/controller/imageController.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import imageModel from "../models/imageModel.js";
 
 // Upload image to cloudinary
 const uploadImage = async (req, res) => {
@@ -13,32 +14,43 @@ const uploadImage = async (req, res) => {
     try {
       // Upload the image
       const result = await cloudinary.uploader.upload(req.file.path, options);
-      console.log(result);
-      if (result) {
-        res.status(200).json({
-          message: "Image uploaded successfully",
-          Image: {
-            asset_id: result.asset_id,
-            public_id: result.public_id,
-            version: result.version,
-            version_id: result.version_id,
-            signature: result.signature,
-            width: result.width,
-            height: result.height,
-            format: result.format,
-            resource_type: result.resource_type,
-            created_at: result.created_at,
-            bytes: result.bytes,
-            type: result.type,
-            etag: result.etag,
-            placeholder: result.placeholder,
-            url: result.url,
-            secure_url: result.secure_url,
-            folder: result.folder,
-            original_filename: result.original_filename,
-          },
-        });
-      }
+      // Remove unused variables from returned value
+      delete result[("api_key", "tags")];
+      // Create image object
+      const image = new imageModel({
+        ...result,
+        related_location: "6551fb6679b950ffc7a6ea48",
+      });
+      const savedImage = await image.save();
+      res.status(200).json({
+        result,
+      });
+      // console.log(result);
+      // if (result) {
+      //   res.status(200).json({
+      //     message: "Image uploaded successfully",
+      //     Image: {
+      //       asset_id: result.asset_id,
+      //       public_id: result.public_id,
+      //       version: result.version,
+      //       version_id: result.version_id,
+      //       signature: result.signature,
+      //       width: result.width,
+      //       height: result.height,
+      //       format: result.format,
+      //       resource_type: result.resource_type,
+      //       created_at: result.created_at,
+      //       bytes: result.bytes,
+      //       type: result.type,
+      //       etag: result.etag,
+      //       placeholder: result.placeholder,
+      //       url: result.url,
+      //       secure_url: result.secure_url,
+      //       folder: result.folder,
+      //       original_filename: result.original_filename,
+      //     },
+      //   });
+      // }
     } catch (error) {
       console.error(error);
     }
---
+++ server/models/imageModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const imageSchema = new mongoose.Schema({
+  asset_id: { type: String, required: true },
+  public_id: { type: String, required: true },
+  version: { type: Number, required: true },
+  version_id: { type: String, required: true },
+  signature: { type: String, required: true },
+  width: { type: Number, required: true },
+  height: { type: Number, required: true },
+  format: { type: String, required: true },
+  resource_type: { type: String, required: true },
+  created_at: { type: String, required: true },
+  bytes: { type: Number, required: true },
+  type: { type: String, required: true },
+  etag: { type: String, required: true },
+  placeholder: { type: Boolean, required: true },
+  url: { type: String, required: true },
+  secure_url: { type: String, required: true },
+  folder: { type: String, required: true },
+  original_filename: { type: String, required: true },
+  related_user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  related_location: { type: mongoose.Schema.Types.ObjectId, ref: "location" },
+});
+
+const imageModel = mongoose.model("image", imageSchema);
+
+export default imageModel;
