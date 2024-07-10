2d39709fabc643739a148ef9c71f334227c37e18
RZajacc <rf.zajac@gmail.com>
2024-07-10 16:18:53 +0200

    adjusted styling of LoadingView for a bigger screen

--- client/src/_components/ui/LoadingView.tsx
+++ client/src/_components/ui/LoadingView.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 function LoadingView({}: Props) {
   return (
-    <div className="mt-8 grid w-full animate-pulse gap-x-4 gap-y-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="mx-auto mt-8 grid w-full max-w-3xl animate-pulse gap-x-4 gap-y-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <PreviewedElement />
       <PreviewedElement />
       <PreviewedElement />
