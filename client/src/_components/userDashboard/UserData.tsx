ad6455d0f8ad328ca2fd1b6496b6d9852c94f9a1
RZajacc <rf.zajac@gmail.com>
2024-07-11 19:59:31 +0200

    added edit icon to fields that I plan to edit

--- client/src/_components/userDashboard/UserData.tsx
+++ client/src/_components/userDashboard/UserData.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import pencil from '../../assets/pencil.svg';
+import confirm from '../../assets/confirm-svgrepo-com.svg';
+import Image from 'next/image';
 
 type Props = {
   propName: string;
@@ -6,10 +9,17 @@ type Props = {
 };
 
 function UserData({ propName, propValue }: Props) {
+  let renderIcon = undefined;
+
+  if (propName !== 'Email adress:') {
+    renderIcon = <Image src={pencil} alt="pencil" className="mx-auto w-5" />;
+  }
+
   return (
-    <article className="grid grid-cols-5">
+    <article className="grid grid-cols-6">
       <span className="col-span-2 font-bold">{propName}</span>
       <span className="col-span-3">{propValue}</span>
+      <span>{renderIcon}</span>
     </article>
   );
 }
