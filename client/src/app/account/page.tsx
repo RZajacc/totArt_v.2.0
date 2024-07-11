32d4aefc69e0cd39ed0deaac4c0a18a851910785
RZajacc <rf.zajac@gmail.com>
2024-07-11 17:38:17 +0200

    cleaned up account view from old code that is not used anymore

--- client/src/app/account/page.tsx
+++ client/src/app/account/page.tsx
@@ -21,11 +21,6 @@ function Account() {
     renderedComponent = <UserPosts />;
   }
 
-  // ! TYPESCRIPT ON EVENT!
-  // const userNavHandle = (e) => {
-  //   setActiveComponent(e.target.innerText);
-  // };
-
   return (
     <>
       <div className="rounded-md bg-green-400 p-2 shadow-md shadow-gray-800">
@@ -45,45 +40,6 @@ function Account() {
         </ul>
         {renderedComponent}
       </div>
-      {/* <Container className="dashboard-container">
-        <Row className="justify-content-center text-center">
-          <Col className="dashboard-nav-column" xs={2}>
-            <Row>
-              <p className="dashboard-nav-link" onClick={userNavHandle}>
-                User profile
-              </p>
-            </Row>
-            <Row>
-              <p className="dashboard-nav-link" onClick={userNavHandle}>
-                Update profile
-              </p>
-            </Row>
-            <Row>
-              <p className="dashboard-nav-link" onClick={userNavHandle}>
-                Favourites
-              </p>
-            </Row>
-            <Row>
-              <p className="dashboard-nav-link" onClick={userNavHandle}>
-                Your posts
-              </p>
-            </Row>
-          </Col>
-          <Col className="dashboard-data-column" xs={6}>
-            {activeComponent === 'User profile' ? (
-              <UserProfile />
-            ) : activeComponent === 'Update profile' ? (
-              <UserUpdate />
-            ) : activeComponent === 'Favourites' ? (
-              <UserFavs />
-            ) : activeComponent === 'Your posts' ? (
-              <UserPosts />
-            ) : (
-              ''
-            )}
-          </Col>
-        </Row>
-      </Container> */}
     </>
   );
 }
