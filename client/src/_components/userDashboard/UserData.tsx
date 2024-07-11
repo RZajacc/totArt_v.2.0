35a325fe4592a3aca337b0db18f991d69a34ef4b
RZajacc <rf.zajac@gmail.com>
2024-07-11 20:39:40 +0200

    improved a conditional render in userData. Now after clicking pencil input field appears at level of relevant field with a value from the server. Its also editable but confirming doesnt save the value yet

--- client/src/_components/userDashboard/UserData.tsx
+++ client/src/_components/userDashboard/UserData.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import pencil from '../../assets/pencil.svg';
 import confirm from '../../assets/confirm-svgrepo-com.svg';
 import Image from 'next/image';
@@ -9,17 +9,46 @@ type Props = {
 };
 
 function UserData({ propName, propValue }: Props) {
-  let renderIcon = undefined;
+  const [isEdited, setIsEdited] = useState(false);
+  const [propState, setPropState] = useState(propValue);
 
-  if (propName !== 'Email adress:') {
+  // Prepare the value to render
+  let renderIcon = undefined;
+  let valueField = <span className="col-span-3">{propValue}</span>;
+
+  if (propName !== 'Email adress:' && !isEdited) {
     renderIcon = <Image src={pencil} alt="pencil" className="mx-auto w-5" />;
+    valueField = <span className="col-span-3">{propValue}</span>;
+  } else if (propName !== 'Email adress:' && isEdited) {
+    renderIcon = <Image src={confirm} alt="pencil" className="w-5" />;
+    valueField = (
+      <input
+        type="text"
+        value={propState}
+        onChange={(e) => {
+          setPropState(e.target.value);
+        }}
+        className="col-span-3 rounded-md border-2 border-black px-1 outline-none focus-within:border-blue-500"
+      />
+    );
   }
 
+  // Handle button state
+  const handleIsEdited = () => {
+    setIsEdited((prevState) => {
+      const newState = !prevState;
+      //TODO If newState is truthy update value with new prop state
+      return newState;
+    });
+  };
+
   return (
-    <article className="grid grid-cols-6">
+    <article className="grid grid-cols-6 items-center">
       <span className="col-span-2 font-bold">{propName}</span>
-      <span className="col-span-3">{propValue}</span>
-      <span>{renderIcon}</span>
+      {valueField}
+      <span className="text-center">
+        <button onClick={handleIsEdited}>{renderIcon}</button>
+      </span>
     </article>
   );
 }
