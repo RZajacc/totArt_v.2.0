74c9dd551b931af1011755ed9401b40b38547777
RZajacc <rf.zajac@gmail.com>
2024-07-26 13:50:31 +0200

    moved all logic and page structure from main locations page into locations card component

--- client/src/_components/locations/LocationCard.tsx
+++ client/src/_components/locations/LocationCard.tsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useContext } from 'react';
 
-type Props = {};
+import emptyHeart from '../../assets/heart_empty.svg';
+import fullHeart from '../../assets/heart_full.svg';
+import { AuthContext } from '../../context/AuthContext';
+import useSWRMutation from 'swr/mutation';
+import { locationFavsData } from '../../fetchers/LocationFavsData';
+import { ErrorView } from '../ui/ErrorView';
+import { locationData } from '../../types/LocationTypes';
 
-function LocationCard({}: Props) {
-  return <div>LocationCard</div>;
+type Props = {
+  locationData: locationData;
+};
+
+function LocationCard({ locationData }: Props) {
+  const { user, mutateUser } = useContext(AuthContext);
+  // Mutation to trigger on upon button click
+
+  const { trigger: triggerFavsHandler, error: favshandlerError } =
+    useSWRMutation(
+      'http://localhost:5000/api/users/handleFavouriteLocations',
+      locationFavsData,
+    );
+
+  if (favshandlerError) {
+    return <ErrorView error={favshandlerError} />;
+  }
+
+  const handleFavourites = async (locId: string) => {
+    await triggerFavsHandler({ email: user!.email, locactionId: locId });
+    mutateUser();
+  };
+  return (
+    <div
+      key={locationData._id}
+      className="rounded-lg border-2 border-black shadow-md shadow-black"
+    >
+      <section className="relative">
+        {user ? (
+          user.favs?.includes(locationData._id) ? (
+            <button
+              className="absolute right-2 top-2"
+              onClick={() => {
+                handleFavourites(locationData._id);
+              }}
+            >
+              <Image src={fullHeart} width={42} height={42} alt="empty-heart" />
+            </button>
+          ) : (
+            <button
+              className="absolute right-2 top-2"
+              onClick={() => {
+                handleFavourites(locationData._id);
+              }}
+            >
+              <Image
+                src={emptyHeart}
+                width={42}
+                height={42}
+                alt="empty-heart"
+              />
+            </button>
+          )
+        ) : (
+          ''
+        )}
+
+        {/* For now Im using some arbitrary values untill I update image info in DB */}
+        <Image
+          src={locationData.image.secure_url}
+          alt={locationData.title}
+          width={600}
+          height={600}
+          layout="responsive"
+          className="rounded-lg"
+        />
+      </section>
+      <section className="mb-4 mt-1 text-center">
+        <h3 className="mb-3 text-xl font-bold">{locationData.title}</h3>
+        {/* Temporary location until connecting view is ready*/}
+        <Link
+          href={`/locations/${locationData._id}`}
+          className="rounded-md bg-black px-3 py-2 text-white"
+        >
+          See more
+        </Link>
+      </section>
+    </div>
+  );
 }
 
 export default LocationCard;
--- client/src/app/locations/page.tsx
+++ client/src/app/locations/page.tsx
@@ -1,28 +1,16 @@
 'use client';
-import Image from 'next/image';
-import Link from 'next/link';
 import useSWR from 'swr';
 import { getAllLocations } from '../../fetchers/GetAllLocations';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
-import emptyHeart from '../../assets/heart_empty.svg';
-import fullHeart from '../../assets/heart_full.svg';
-import useSWRMutation from 'swr/mutation';
-import { locationFavsData } from '../../fetchers/LocationFavsData';
 import AddLocationModal from '../../_components/locationModals/AddLocationModal';
 import { ErrorView } from '../../_components/ui/ErrorView';
 import LoadingView from '../../_components/ui/LoadingView';
+import LocationCard from '../../_components/locations/LocationCard';
 
 function Content() {
-  const { user, mutateUser } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const [showAddLocation, setShowAddLocation] = useState(false);
-
-  // Mutation to trigger on upon button click
-  const { trigger: triggerFavsHandler, error: favshandlerError } =
-    useSWRMutation(
-      'http://localhost:5000/api/users/handleFavouriteLocations',
-      locationFavsData,
-    );
 
   const {
     data: locations,
@@ -38,15 +26,6 @@ function Content() {
   if (locationsLoading) {
     return <LoadingView />;
   }
-
-  if (favshandlerError) {
-    return <ErrorView error={favshandlerError} />;
-  }
-
-  const handleFavourites = async (locId: string) => {
-    await triggerFavsHandler({ email: user!.email, locactionId: locId });
-    mutateUser();
-  };
 
   return (
     <>
@@ -74,69 +53,8 @@ function Content() {
 
       <div className="mx-auto mt-3 grid max-w-3xl gap-3 sm:grid-cols-2 md:grid-cols-3">
         {locations &&
-          locations.locations.map((loc) => {
-            return (
-              <div
-                key={loc._id}
-                className="rounded-lg border-2 border-black shadow-md shadow-black"
-              >
-                <section className="relative">
-                  {user ? (
-                    user.favs?.includes(loc._id) ? (
-                      <button
-                        className="absolute right-2 top-2"
-                        onClick={() => {
-                          handleFavourites(loc._id);
-                        }}
-                      >
-                        <Image
-                          src={fullHeart}
-                          width={42}
-                          height={42}
-                          alt="empty-heart"
-                        />
-                      </button>
-                    ) : (
-                      <button
-                        className="absolute right-2 top-2"
-                        onClick={() => {
-                          handleFavourites(loc._id);
-                        }}
-                      >
-                        <Image
-                          src={emptyHeart}
-                          width={42}
-                          height={42}
-                          alt="empty-heart"
-                        />
-                      </button>
-                    )
-                  ) : (
-                    ''
-                  )}
-
-                  {/* For now Im using some arbitrary values untill I update image info in DB */}
-                  <Image
-                    src={loc.image.secure_url}
-                    alt={loc.title}
-                    width={600}
-                    height={600}
-                    layout="responsive"
-                    className="rounded-lg"
-                  />
-                </section>
-                <section className="mb-4 mt-1 text-center">
-                  <h3 className="mb-3 text-xl font-bold">{loc.title}</h3>
-                  {/* Temporary location until connecting view is ready*/}
-                  <Link
-                    href={`/locations/${loc._id}`}
-                    className="rounded-md bg-black px-3 py-2 text-white"
-                  >
-                    See more
-                  </Link>
-                </section>
-              </div>
-            );
+          locations.locations.map((location) => {
+            return <LocationCard key={location._id} locationData={location} />;
           })}
       </div>
     </>
