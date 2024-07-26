32855c1774e378623b35b1707784e4b3fb7edbba
RZajacc <rf.zajac@gmail.com>
2024-07-26 16:14:41 +0200

    displayed in a simple form data for user locations

--- client/src/_components/userDashboard/UserLocations.tsx
+++ client/src/_components/userDashboard/UserLocations.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+
 function UserLocations() {
-  // const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <>
       <h1>User locations</h1>
+      {user?.posts.map((location) => {
+        return <p>{location.title}</p>;
+      })}
     </>
   );
 }
