9efa70a1bd99bcf37013ebaededc16abd4e7a842
RZajacc <rf.zajac@gmail.com>
2024-07-26 16:35:20 +0200

    copied and adjusted a bit elements from userFavs to userLocations since it will be more or less same. Also created LocationItem component

--- client/src/_components/userDashboard/UserLocations.tsx
+++ client/src/_components/userDashboard/UserLocations.tsx
@@ -4,12 +4,29 @@ import { AuthContext } from '../../context/AuthContext';
 function UserLocations() {
   const { user } = useContext(AuthContext);
 
+  // Renderable elements
+  const noFavs = (
+    <p className="text-center">You didn't create any locations yet...</p>
+  );
+
+  const header = (
+    <h4 className="text-center font-bold">
+      You've created <span className="text-red-500">{user?.posts.length}</span>{' '}
+      location so far:
+    </h4>
+  );
+
   return (
     <>
-      <h1>User locations</h1>
-      {user?.posts.map((location) => {
-        return <p>{location.title}</p>;
-      })}
+      <div className="mx-auto w-11/12 rounded-sm bg-slate-200 px-4 py-2">
+        {user!.favs.length > 0 ? header : noFavs}
+        <ol className="list-inside list-decimal">
+          {user!.posts &&
+            user?.posts.map((location) => {
+              return <p>{location.title}</p>;
+            })}
+        </ol>
+      </div>
     </>
   );
 }
