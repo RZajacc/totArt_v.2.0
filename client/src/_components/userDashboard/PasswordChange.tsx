4e0dd721886cc0e1a689642643d4a2b7f4d484ba
RZajacc <rf.zajac@gmail.com>
2024-07-29 14:49:09 +0200

    applied logic to properly render passwordChange and DeleteField components depending on option selected

---
+++ client/src/_components/userDashboard/PasswordChange.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+function PasswordChange({}: Props) {
+  return <div>PasswordChange</div>;
+}
+
+export default PasswordChange;
--- client/src/_components/userDashboard/ProfileActions.tsx
+++ client/src/_components/userDashboard/ProfileActions.tsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PasswordChange from './PasswordChange';
+import DeleteField from '../ui/DeleteField';
 
 type Props = {};
 
 function ProfileActions({}: Props) {
+  const [showPasswordChange, setShowPasswordChange] = useState(false);
+  const [showDeleteAccount, setShowDeleteAccount] = useState(false);
+
+  const actionsHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    if (e.currentTarget.value === 'password-change') {
+      setShowPasswordChange((prevState) => {
+        if (showDeleteAccount) {
+          setShowDeleteAccount(false);
+        }
+        return !prevState;
+      });
+    } else if (e.currentTarget.value === 'delete-account') {
+      setShowDeleteAccount((prevState) => {
+        if (showPasswordChange) {
+          setShowPasswordChange(false);
+        }
+        return !prevState;
+      });
+    }
+  };
   return (
     <>
       <div className="my-2 flex justify-around rounded-sm p-1">
-        <button className="rounded-lg border-2 border-black bg-stone-300 p-1 px-2 shadow-md shadow-black hover:font-bold">
+        <button
+          className="rounded-lg border-2 border-black bg-stone-300 p-1 px-2 shadow-md shadow-black hover:font-bold"
+          value={'password-change'}
+          onClick={actionsHandler}
+        >
           Change password
         </button>
-        <button className="rounded-lg border-2 border-black bg-red-500 p-1 px-2 text-stone-300 shadow-md shadow-black hover:font-bold">
+        <button
+          className="rounded-lg border-2 border-black bg-red-500 p-1 px-2 text-stone-300 shadow-md shadow-black hover:font-bold"
+          value={'delete-account'}
+          onClick={actionsHandler}
+        >
           Delete account
         </button>
       </div>
+      {showPasswordChange && <PasswordChange />}
+      {showDeleteAccount && <DeleteField />}
     </>
   );
 }
