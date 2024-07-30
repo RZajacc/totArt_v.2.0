4dd1649d0ca1d8f6f080b5cc4579b461f750add5
RZajacc <rf.zajac@gmail.com>
2024-07-30 14:59:35 +0200

    implemented validation of a new password field with custom validation function and a list with a feedback to the user

--- client/src/_components/userDashboard/PasswordChange.tsx
+++ client/src/_components/userDashboard/PasswordChange.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PasswordField from '../ui/PasswordField';
 import useSWRMutation from 'swr/mutation';
 import { VerifyUserPassword } from '../../fetchers/VerifyUserPassword';
+import { validatePassword } from '../../utils/ValidatePassword';
 
 type Props = {};
 
@@ -9,11 +10,20 @@ function PasswordChange({}: Props) {
   // Password validation state variables
   const [isCurrentPswValid, setIsCurrentPswValid] = useState(true);
   const [isNewPswValid, setIsNewPswValid] = useState(true);
+  const [newPswErrorList, setNewPswErrorList] = useState<string[]>([]);
 
   // Renderable elements
   const currentPswInvalidParagraph = (
     <p className="text-red-400">Password is incorrect!</p>
   );
+  const newPswErrorParagraph = (
+    <ol className="list-inside list-disc px-1 text-red-400">
+      {newPswErrorList.map((err, idx) => {
+        return <li key={idx}>{err}</li>;
+      })}
+    </ol>
+  );
+
   // Methods to validate and update user password
   const { trigger: triggerPswVerification } = useSWRMutation(
     'http://localhost:5000/api/users/verifyUserPassword',
@@ -25,6 +35,8 @@ function PasswordChange({}: Props) {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const currentPassword = formData.get('current-password') as string;
+    const newPassword = formData.get('new-password') as string;
+    const confirmPassword = formData.get('confirm-password') as string;
 
     // Verify users current password
     const verifyPassword = await triggerPswVerification({
@@ -32,10 +44,18 @@ function PasswordChange({}: Props) {
       password: currentPassword,
     });
 
+    // Check if current password is valid
     if (!verifyPassword.passwordValid) {
       setIsCurrentPswValid(false);
     } else {
       setIsCurrentPswValid(true);
+      // Check if new password is valid
+      const validNewPsw = validatePassword(newPassword, confirmPassword);
+      // Method returns a list of elements
+      if (validNewPsw.length !== 0) {
+        setIsNewPswValid(false);
+        setNewPswErrorList(validNewPsw);
+      }
     }
   };
   return (
@@ -53,13 +73,16 @@ function PasswordChange({}: Props) {
       <PasswordField
         labelName="new-password"
         labelValue=" New password:"
-        isValid={true}
+        isValid={isNewPswValid}
+        setIsValid={setIsNewPswValid}
       />
       <PasswordField
         labelName="confirm-password"
         labelValue="Repeat password:"
-        isValid={false}
+        isValid={isNewPswValid}
+        setIsValid={setIsNewPswValid}
       />
+      {!isNewPswValid && newPswErrorParagraph}
       <button className="my-2 rounded-sm bg-black p-2 text-white shadow-md shadow-gray-600">
         Submit
       </button>
