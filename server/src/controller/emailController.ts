1f1e43bb84f6efdbaa73781ba182457aeddce9ae
RZajacc <rf.zajac@gmail.com>
2024-08-02 12:03:41 +0200

    installed nodemailer types

--- server/src/controller/emailController.ts
+++ server/src/controller/emailController.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import nodemailer from "nodemailer";
 
 const sendEmailTest: RequestHandler = async (req, res) => {
   res.status(200).json({ msg: "Hello" });
