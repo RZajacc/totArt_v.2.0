38051024ca5322a42c9368dd3cd57ad0330da4a3
RZajacc <rf.zajac@gmail.com>
2024-08-02 12:27:52 +0200

    added nodemailer sendgrid to the project

--- server/src/controller/emailController.ts
+++ server/src/controller/emailController.ts
@@ -2,7 +2,25 @@ import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
 
 const sendEmailTest: RequestHandler = async (req, res) => {
-  res.status(200).json({ msg: "Hello" });
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: { user: "rf.zajac@gmail.com", pass: "Kkrulik#2" },
+  });
+
+  let mailOptions = {
+    from: "rf.zajac@gmail.com",
+    to: "rf.zajac@gmail.com",
+    subject: "Test email",
+    text: "Test email content",
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent:" + info.response);
+    }
+  });
 };
 
 export { sendEmailTest };
