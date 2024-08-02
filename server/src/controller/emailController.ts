eca5e460ecf0d4ca8155613f49267eb3ba2b07c3
RZajacc <rf.zajac@gmail.com>
2024-08-02 14:16:09 +0200

    successfully applied sendgrid on the backend and cleared dependencies to nodeMailer that in the end will not be used

--- server/src/controller/emailController.ts
+++ server/src/controller/emailController.ts
@@ -1,26 +1,28 @@
 import { RequestHandler } from "express";
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey("");
 
 const sendEmailTest: RequestHandler = async (req, res) => {
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { user: "rf.zajac@gmail.com", pass: "Kkrulik#2" },
-  });
-
-  let mailOptions = {
-    from: "rf.zajac@gmail.com",
-    to: "rf.zajac@gmail.com",
-    subject: "Test email",
-    text: "Test email content",
+  const msg = {
+    to: "rf.zajac@gmail.com", // Change to your recipient
+    from: {
+      email: "rf.zajac@tutamail.com",
+      name: "totArt",
+    },
+    subject: "Sending with SendGrid is Fun",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent:" + info.response);
-    }
-  });
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode);
+      console.log(response[0].headers);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 export { sendEmailTest };
