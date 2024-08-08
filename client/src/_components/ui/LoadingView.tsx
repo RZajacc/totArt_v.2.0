b5184880d263eccac7e5f906108a8a53a9cb863b
RZajacc <rf.zajac@gmail.com>
2024-08-08 15:29:11 +0200

    style update to message upon login

--- client/src/_components/ui/LoadingView.tsx
+++ client/src/_components/ui/LoadingView.tsx
@@ -6,7 +6,7 @@ type Props = {};
 function LoadingView({}: Props) {
   return (
     <div className="mx-auto mt-8 grid w-full max-w-3xl animate-pulse gap-x-4 gap-y-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <div className="col-span-2">
+      <div className="md:col-span-2 lg:col-span-3 xl:col-span-4">
         <p className="my-1 text-center">
           Backend is running on a free tier and might require some time to start
           after being inactive but it works!✌️
