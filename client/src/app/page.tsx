31cb2b885ffd30a0c12d5f92fc33a378a019baf0
RZajacc <rf.zajac@gmail.com>
2024-08-09 12:30:46 +0200

    fixed few typos in home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,23 +8,23 @@ function Home() {
       </h1>
       <section className="mt-3">
         <p>
-          So to begin with what where does this name comes from? Well It comes
-          from a name of a simple psychological phenomenon when you try to
-          recall something and you feel like you almost have it but not really.
-          It is called a tip of the tongue phenomenon.
+          So to begin with, where does this name comes from? Well It comes from
+          a name of a simple psychological phenomenon when you try to recall
+          something and you feel like you almost have it but not really. It is
+          called a tip of the tongue phenomenon.
         </p>
         <br />
         <p>
           It applies for words and informations, but here I will use it in a
           different context. How many times have you walked around the city,
-          seen a cool place and thoght "cool, I should definitely remember this
-          place", and the next they you forget that it ever existed?
+          seen a cool place and thought "cool, I should definitely remember this
+          place", and a minute after you forget that it ever existed?
         </p>
         <br />
         <p>
           If this feeling is familiar to you&apos;ve came to the right place.
           You can document those places here or just watch, what other people
-          shared! Hope you&apos;ll enjoy it! Tp start browsing the content just
+          shared! Hope you&apos;ll enjoy it! To start browsing the content just
           click{' '}
           <Link href={'/locations'} className="font-bold hover:animate-pulse">
             here
