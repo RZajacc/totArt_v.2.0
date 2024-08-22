adbbb9d9c711c5cfdb80378e24e3b59cbcb991ce
RZajacc <rf.zajac@gmail.com>
2024-08-22 15:22:55 +0200

    fixed small issue in isAuth component. Now its basing authentication on user object, not on data stored in localStorage

--- client/src/utils/IsAuth.tsx
+++ client/src/utils/IsAuth.tsx
@@ -1,20 +1,21 @@
 'use client';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { redirect } from 'next/navigation';
+import { AuthContext } from '../context/AuthContext';
 
 export default function isAuth(Component: any) {
   return function IsAuth(props: any) {
     // Get user auth token
-    const auth = localStorage.getItem('auth_token');
+    const { user } = useContext(AuthContext);
 
     // If it is stored in the browser continue but if not redirect to login
     useEffect(() => {
-      if (!auth) {
+      if (!user) {
         return redirect('/login');
       }
     }, []);
 
-    if (!auth) {
+    if (!user) {
       return null;
     }
 
