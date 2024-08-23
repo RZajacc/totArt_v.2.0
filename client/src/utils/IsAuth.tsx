610c1b7ae1c9092261ae62a54634b22b8fb9fc87
RZajacc <rf.zajac@gmail.com>
2024-08-23 10:27:20 +0200

    cleaned up isAuth component from unnecessary if check

--- client/src/utils/IsAuth.tsx
+++ client/src/utils/IsAuth.tsx
@@ -4,18 +4,17 @@ import { redirect } from 'next/navigation';
 
 export default function isAuth(Component: any) {
   return function IsAuth(props: any) {
-    // If it is stored in the browser continue but if not redirect to login
     const [auth, setAuth] = useState<string | null>(null);
 
+    // To prevent SSR page loading when user type adress manually token is acquired in useEffect
     useEffect(() => {
       // Get user auth token
-      if (typeof window !== 'undefined') {
-        const token = localStorage.getItem('auth_token');
-        setAuth(token);
-
-        if (!token) {
-          return redirect('/login');
-        }
+      const token = localStorage.getItem('auth_token');
+      // Assign it to useState
+      setAuth(token);
+      // If user is not authenticated redirect to login
+      if (!token) {
+        return redirect('/login');
       }
     }, []);
 
