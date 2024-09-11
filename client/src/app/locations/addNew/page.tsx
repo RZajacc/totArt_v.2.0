a54add8b0d86b293d3c56408255608dea9f00006
RZajacc <rf.zajac@gmail.com>
2024-09-11 15:04:49 +0200

    Applied adding new location logic fully in a new component

--- client/src/app/locations/addNew/page.tsx
+++ client/src/app/locations/addNew/page.tsx
@@ -1,25 +1,107 @@
 'use client';
 // Libraries
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import useSWRMutation from 'swr/mutation';
+import { useRouter } from 'next/navigation';
 // Components
 import ImagePicker from '@/_components/locations/ImagePicker';
 import isAuth from '@/utils/IsAuth';
 import LabeledInput from '@/_components/formElements/LabeledInput';
 import LabeledTextArea from '@/_components/formElements/LabeledTextArea';
 import ButtonDark from '@/_components/formElements/ButtonDark';
+// Fetchin data
+import { ImageUpload } from '@/fetchers/ImageUpload';
+import { deleteImage } from '@/fetchers/DeleteImage';
+import { addNewLocation } from '@/fetchers/AddNewLocation';
+// Context data
+import { AuthContext } from '@/context/AuthContext';
 
-type Props = {};
+function page() {
+  // Getting user data
+  const { user } = useContext(AuthContext);
 
-function page({}: Props) {
+  // Image picker data
   const [selectedImage, setSelectedImage] = useState<
     string | ArrayBuffer | null
   >(null);
 
+  // Router instance to redirect after submission
+  const router = useRouter();
+
+  // SWR Methods necessary for all operations
+  const {
+    data: imageData,
+    trigger: triggerImageUpload,
+    isMutating: imageIsMutating,
+    error: imageUploadError,
+  } = useSWRMutation(
+    `${process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://totart-v-2-0.onrender.com'}/api/images/ImageUpload`,
+    ImageUpload,
+  );
+
+  const { trigger: triggerDeletingImage } = useSWRMutation(
+    `${process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://totart-v-2-0.onrender.com'}/api/images/imageDelete`,
+    deleteImage,
+  );
+
+  const {
+    trigger: triggerAddLocation,
+    isMutating: addingLocation,
+    error: addLocationError,
+  } = useSWRMutation(
+    `${process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://totart-v-2-0.onrender.com'}/api/locations/addNewLocation`,
+    addNewLocation,
+  );
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Define a form
+    const form = event.currentTarget;
+
+    // Get the file from submitted in the form
+    const formData = new FormData(form);
+    const imageFile = formData.get('locationImage') as File;
+    const title = formData.get('title') as string;
+    const description = formData.get('description') as string;
+    const location = formData.get('location') as string;
+
+    // If previous submission failed, but image was uploaded delete it
+    if (imageData) {
+      await triggerDeletingImage({
+        imageId: imageData._id,
+        publicId: imageData.public_id,
+      });
+    }
+    // Upload an image to cloudinary
+    const imageResponse = await triggerImageUpload({
+      file: imageFile,
+      folder: 'postImages',
+    });
+
+    // If uploading image failed stop execution
+    if (!imageResponse) {
+      return;
+    }
+
+    // Submit new location
+    await triggerAddLocation({
+      author: user ? user._id : '',
+      title: title,
+      description: description,
+      location: location,
+      imageId: imageResponse._id,
+    });
+
+    // Redirect client side
+    router.push('/locations');
+  };
+
   return (
     <div className="mx-auto mt-5 max-w-lg rounded-md bg-gradient-to-br from-green-300 to-green-500 p-2 shadow-md shadow-black">
-      <h1 className="text-center font-bold">Share some unique content:</h1>
+      <h1 className="mb-3 text-center font-bold">Share some unique content:</h1>
 
-      <form className="grid gap-y-2">
+      <form className="grid gap-y-2" onSubmit={handleSubmit}>
         <LabeledInput
           inputType="text"
           labelFor="title"
@@ -43,7 +125,15 @@ function page({}: Props) {
           selectedImage={selectedImage}
           setSelectedImage={setSelectedImage}
         />
-        <ButtonDark type="submit">Submit data</ButtonDark>
+        <p className="text-center font-bold text-red-500">
+          {addLocationError ||
+            (imageUploadError && 'Submitting the data failed! Try again!')}
+        </p>
+        <ButtonDark type="submit" disabled={imageIsMutating || addingLocation}>
+          {imageIsMutating || addingLocation
+            ? 'Submitting the form...'
+            : 'Submit data'}
+        </ButtonDark>
       </form>
     </div>
   );
