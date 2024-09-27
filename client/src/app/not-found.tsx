0fce5506398a27669e8945cc42db990b86cc2a25
RZajacc <rf.zajac@gmail.com>
2024-09-27 15:08:46 +0200

    added a very simple not-found page

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="mx-auto mt-40 max-w-md text-center">
+      <h2 className="text-2xl font-bold">Page not found!</h2>
+      <p>Seems like you typed a wrong adress!</p>
+    </div>
+  );
+}
+
+export default NotFound;
