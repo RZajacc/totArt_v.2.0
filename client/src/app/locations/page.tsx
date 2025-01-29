78051478f7b7f71cb085710c7217941ff05c5100
RZajacc <rf.zajac@gmail.com>
2025-01-29 16:41:36 +0100

    moved fetch urls into .env.local so they can be overwritten by docker but also leave some flexibility to run the project locally

--- client/src/app/locations/page.tsx
+++ client/src/app/locations/page.tsx
@@ -12,10 +12,16 @@ export const metadata: Metadata = {
 };
 
 async function Content() {
+  // Define if local or remote URL should be used (Remote is necessary for production only)
+  const LOCAL_URL = process.env.NEXT_PUBLIC_API_URL;
+  const REMOTE_URL = process.env.NEXT_PUBLIC_REMOTE_API_URL;
+
+  // Build fetch URL
+  const FETCH_URL =
+    process.env.NODE_ENV === 'development' ? LOCAL_URL : REMOTE_URL;
+
   // Fetch locations data
-  const response = await fetch(
-    `${process.env.NODE_ENV === 'development' ? 'http://backend:5000' : 'https://totart-v-2-0.onrender.com'}/api/locations/all`,
-  );
+  const response = await fetch(`${FETCH_URL}/api/locations/all`);
 
   // If fetching fails display error
   if (!response.ok) {
