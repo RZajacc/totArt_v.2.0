4d0ed7471fe80dcac9caa7df3caf42279559e073
Rafal Zajac <rf.zajac@gmail.com>
2025-02-25 14:21:17 +0100

    updated locations page to catch errors and created an error class which is extending error, and adds status

--- client/src/app/locations/page.tsx
+++ client/src/app/locations/page.tsx
@@ -5,6 +5,7 @@ import { ErrorView } from '@/_components/ui/state/ErrorView';
 // Types
 import type { locationData } from '@/types/locationTypes';
 import { BuildFetchUrl } from '@/utils/BuildFetchUrl';
+import { FetchError } from '@/utils/FetchError';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -16,47 +17,55 @@ async function Content() {
   // Build Fetch url
   const FETCH_URL = BuildFetchUrl();
 
-  // Fetch locations data
-  const response = await fetch(`${FETCH_URL}/api/locations/all`);
+  try {
+    // Fetch locations data
+    const response = await fetch(`${FETCH_URL}/api/locations/all`);
 
-  // If fetching fails display error
-  if (!response.ok) {
+    if (!response.ok) {
+      throw new FetchError('Something went wrong', response.status);
+    }
+
+    // If fetch succeeds get the data
+    const locations: { number: number; locations: locationData[] } =
+      await response.json();
+
     return (
-      <ErrorView
-        error={{
-          info: 'Fetching data failed',
-          status: response.status,
-          name: 'Error',
-          message: 'Something went wrong!',
-        }}
-      />
+      <>
+        <div className="mb-6 mt-3 text-center">
+          <h1 className="mb-4 text-center text-xl font-bold md:mt-4">
+            Number of locations found:{' '}
+            <span data-testid="locations-count" className="text-emerald-500">
+              {locations?.number}
+            </span>
+          </h1>
+          <AddLocationLink />
+        </div>
+
+        <div className="mx-auto mt-3 grid max-w-4xl gap-3 sm:grid-cols-2 md:grid-cols-3">
+          {locations &&
+            locations.locations.map((location) => {
+              return (
+                <LocationCard key={location._id} locationData={location} />
+              );
+            })}
+        </div>
+      </>
     );
+  } catch (error) {
+    if (error instanceof FetchError) {
+      // If fetching fails display error
+      return (
+        <ErrorView
+          error={{
+            info: 'Fetching data failed',
+            status: error.status,
+            name: 'Error',
+            message: error.message,
+          }}
+        />
+      );
+    }
   }
-
-  // If fetch succeeds get the data
-  const locations: { number: number; locations: locationData[] } =
-    await response.json();
-
-  return (
-    <>
-      <div className="mb-6 mt-3 text-center">
-        <h1 className="mb-4 text-center text-xl font-bold md:mt-4">
-          Number of locations found:{' '}
-          <span data-testid="locations-count" className="text-emerald-500">
-            {locations?.number}
-          </span>
-        </h1>
-        <AddLocationLink />
-      </div>
-
-      <div className="mx-auto mt-3 grid max-w-4xl gap-3 sm:grid-cols-2 md:grid-cols-3">
-        {locations &&
-          locations.locations.map((location) => {
-            return <LocationCard key={location._id} locationData={location} />;
-          })}
-      </div>
-    </>
-  );
 }
 
 export default Content;
--- client/src/utils/BuildFetchUrl.tsx
+++ client/src/utils/BuildFetchUrl.tsx
@@ -1,12 +1,7 @@
 export const BuildFetchUrl = () => {
   // Pick url depending if component is SSR or CSR (important for docker only)
-  const LOCAL_URL =
-    process.env.API_URL_SERVER || process.env.NEXT_PUBLIC_API_URL;
-  // Remote URL for deployed version
-  const REMOTE_URL = process.env.NEXT_PUBLIC_REMOTE_API_URL;
-
-  // Build fetch URL
   const FETCH_URL =
-    process.env.NODE_ENV === 'development' ? LOCAL_URL : REMOTE_URL;
+    process.env.API_URL_SERVER || process.env.NEXT_PUBLIC_API_URL;
+
   return FETCH_URL;
 };
